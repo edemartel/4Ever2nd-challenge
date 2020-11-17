c3444a88e2f6bd32a2b716807311123feb72203b
Etienne de Martel <edemartel@gmail.com>
2020-11-17 18:23:45 -0500

    First commit.

---
+++ Bot.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace Blitz
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MyBot : ControllerBase
+    {
+        [HttpPost("/microchallenge")]
+        public ActionResult Solve([FromBody] Problem problem)
+        {
+            var result = new int[problem.items.Length];
+            for (var i = 0; i < problem.items.Length; i++)
+            {
+                var source = problem.items[i][0];
+                var destination = problem.items[i][1];
+
+                if (source < destination)
+                {
+                    for (int j = source; j < destination; ++j)
+                        result[i] += problem.track[j];
+                }
+                else if (source > destination)
+                {
+                    for (int j = source - 1; j >= destination; --j)
+                        result[i] += problem.track[j];
+                }
+            }
+
+            Console.WriteLine(problem);
+            Console.WriteLine(JsonConvert.SerializeObject(result, Formatting.None));
+            Console.WriteLine();
+
+            return Ok(result);
+        }
+    }
+
+    public class Problem
+    {
+        public int[][] items { get; set; }
+        public int[] track { get; set; }
+
+        public override string ToString() {
+            return JsonConvert.SerializeObject(this, Formatting.None);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using CommandLine;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using System.Collections.Generic;
+
+namespace Blitz
+{
+    public class Program
+    {
+        public static int port;
+
+        class Options
+        {
+            [Option('p', "port", HelpText = "Set port number (default: 27178)", Default = 27178)]
+            public int Port { get; set; }
+        }
+
+        public static void Main(string[] args)
+        {
+            Parser.Default.ParseArguments<Options>(args).WithParsed(o =>
+            {
+                port = o.Port;
+            });
+
+            CreateHostBuilder(args).Build().Run();
+        }
+
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                    webBuilder.UseUrls("http://localhost:" + port);
+                    webBuilder.ConfigureAppConfiguration((context, config) =>
+                    {
+                        config.AddInMemoryCollection(new Dictionary<string, string>
+                        {
+                            {"Logging:LogLevel:System", "Error"},
+                            {"Logging:LogLevel:Microsoft", "Error"}
+                        });
+                    });
+                });
+    }
+}
